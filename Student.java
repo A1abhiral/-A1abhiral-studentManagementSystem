352098280a60364224614c9e4451aabda72ae450
abhiral pradhan <111888167+A1abhiral@users.noreply.github.com>
2025-01-14 07:45:08 +0545

    Added getter methods to Student class

--- Student.java
+++ Student.java
@@ -7,7 +7,25 @@ public class Student {
         this.age = age;
     }
 
-  
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void displayStudentInfo() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
 
    
 }
